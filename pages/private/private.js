6882075b7e37fdd651bbc65934ef0c5a3b0ab02c
liuzhixiang <liuzhixiang@mogoroom.com>
2019-03-30 07:47:22 +0800

    update

--- pages/private/private.js
+++ pages/private/private.js
@@ -1,11 +1,15 @@
 // pages/private/private.js
+const app = getApp()
 Page({
 
   /**
    * Page initial data
    */
   data: {
-
+    userInfo: {},
+    hasUserInfo: false,
+    currentTab: 0,
+    mine: []
   },
 
   onGotUserInfo: function (e) {
@@ -18,7 +22,49 @@ Page({
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-
+    wx.cloud.init()
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          clientHeight: res.windowHeight
+        });
+      }
+    });
+    if (app.globalData.userInfo){
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+    console.log(this.data.userInfo)
+    const db = wx.cloud.database()
+    db.collection('qiukua').orderBy('due', 'desc').get({
+      success: res => {
+        this.setData({
+          mine: res.data
+        })
+      }
+    })
   },
 
   /**
@@ -68,5 +114,21 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  bindChange: function (e) {
+    var that = this;
+    that.setData({ currentTab: e.detail.current });
+  },
+
+  swichNav: function (e) {
+    var that = this;
+    if (that.data.currentTab === e.target.dataset.current) {
+      return false;
+    } else {
+      that.setData({
+        currentTab: e.target.dataset.current
+      })
+    }
+  }  
 })
