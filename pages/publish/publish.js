8b4bf71e15f4e7fc30b5a7243c530ab9b8df18dc
liuzhixiang <liuzhixiang@mogoroom.com>
2019-04-13 01:11:41 +0800

    add location

--- pages/publish/publish.js
+++ pages/publish/publish.js
@@ -11,7 +11,8 @@ Page({
     getcontent: null,
     userInfo: {},
     hasUserInfo: false,
-    currentWordNum: 0
+    currentWordNum: 0,
+    userCity: ''
   },
   getinput: function(e) {
     this.data.getinput = e.detail.value;
@@ -25,18 +26,19 @@ Page({
   },
 
   publish: function() {
+    let that = this
     wx.cloud.init()
     if (app.globalData.userInfo)
     {
-      this.setData({
+      that.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse) {
+    } else if (that.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
-        this.setData({
+        that.setData({
           userInfo: res.userInfo,
           hasUserInfo: true
         })
@@ -45,16 +47,15 @@ Page({
       wx.getUserInfo({
         success: res => {
           app.globalData.userInfo = res.userInfo
-          this.setData({
+          that.setData({
             userInfo: res.userInfo,
             hasUserInfo: true
           })
         }
       })
     }
-    var content = this.data.getcontent
-    var value = this.data.getinput
-    var that = this
+    var content = that.data.getcontent
+    var value = that.data.getinput
     if (content == null || content == '') {
       wx.showToast({
         title: '请输入求夸内容',
@@ -73,14 +74,14 @@ Page({
       var util = require('../../utils/util.js')
       db.collection('qiukua').add({
         data: {
-          content: this.data.getcontent,
-          money: this.data.getinput,
+          content: that.data.getcontent,
+          money: that.data.getinput,
           due: util.formatTime(new Date()),
-          nickname: this.data.userInfo.nickName,
-          avatarUrl: this.data.userInfo.avatarUrl,
-          gender: this.data.userInfo.gender,
-          city: this.data.userInfo.city,
-          province: this.data.userInfo.province,
+          nickname: that.data.userInfo.nickName,
+          avatarUrl: that.data.userInfo.avatarUrl,
+          gender: that.data.userInfo.gender,
+          city: that.data.userCity,
+          province: that.data.userInfo.province,
           tags: [
             'cloud',
           ],
@@ -111,7 +112,8 @@ Page({
    * Lifecycle function--Called when page load
    */
   onLoad: function (options) {
-
+    let that = this
+    that.getLocal()
   },
 
   /**
@@ -161,5 +163,91 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  getLocation: function () {
+    let that = this
+    wx.getLocation({
+      type: 'wgs84',
+      success(res) {
+        const latitude = res.latitude
+        const longitude = res.longitude
+        that.getCityName(latitude, longitude)
+      }
+    })
+  },
+  
+  getLocal: function () {
+    let that = this
+    wx.getSetting({
+      success: (res) => {
+        // res.authSetting['scope.userLocation'] == undefined    表示 初始化进入该页面
+        // res.authSetting['scope.userLocation'] == false    表示 非初始化进入该页面,且未授权
+        // res.authSetting['scope.userLocation'] == true    表示 地理位置授权
+        if (res.authSetting['scope.userLocation'] != undefined && res.authSetting['scope.userLocation'] != true) {
+          wx.showModal({
+            title: '请求授权当前位置',
+            content: '需要获取您的地理位置，请确认授权',
+            success: function (res) {
+              if (res.cancel) {
+                wx.showToast({
+                  title: '拒绝授权',
+                  icon: 'none',
+                  duration: 1000
+                })
+              } else if (res.confirm) {
+                wx.openSetting({
+                  success: function (dataAu) {
+                    if (dataAu.authSetting["scope.userLocation"] == true) {
+                      wx.showToast({
+                        title: '授权成功',
+                        icon: 'success',
+                        duration: 1000
+                      })
+                      console.log('heer')
+                      that.getLocation()
+                    } else {
+                      wx.showToast({
+                        title: '授权失败',
+                        icon: 'none',
+                        duration: 1000
+                      })
+                    }
+                  }
+                })
+              }
+            }
+          })
+        } else if (res.authSetting['scope.userLocation'] == undefined) {
+          console.log('zmm1')
+          wx.getLocation({
+            success: function (data) {
+              console.log(data);
+              wx.showToast({ //这里提示失败原因
+                title: '授权成功！',
+                duration: 1500
+              })
+              that.getLocation()
+            }
+          })
+        }
+        else {
+          that.getLocation()
+        }
+      }
+    })
+  },
+  
+  getCityName: function (latitude, longitude) {
+    let that = this
+    wx.request({
+      url: 'https://apis.map.qq.com/ws/geocoder/v1/?location=' + latitude + ',' + longitude + '&key=SB7BZ-6VKHO-LX4WZ-S2H4X-3DBG5-BCBLE',
+      data: {},
+      success: function (res) {
+        that.data.userCity = res.data.result.address_component.city
+      },
+      fail: function (res) {
+      }
+    })
   }
 })
